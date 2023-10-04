53c00b1798ea182ae365e3f3d4164506b77f967b
Ene AL <kaixo.eneal@gmail.com>
2023-10-04 20:39:19 +0200

    fix data image

--- src/components/pages/data.js
+++ src/components/pages/data.js
@@ -1,9 +1,4 @@
-import cactusone from "../../../static/assets/images/cactusone.jpg";
-import cactustwo from "../../../static/assets/images/cactustwo.jpg";
-import cactusthree from "../../../static/assets/images/cactusthree.jpg";
-import cactusfour from "../../../static/assets/images/cactusfour.jpg";
-import cactusfive from "../../../static/assets/images/cactusfive.jpg";
-import cactussix from "../../../static/assets/images/cactussix.jpg";
+
 
 //These data are used as a database of the Store's products.
 
@@ -12,42 +7,42 @@ const data = [
     id: 1,
     name: "Pink Flower",
     price: "10€",
-    imageUrl: cactusone,
+    imageUrl: "https://images2.imgbox.com/3a/25/C8EiYTiB_o.jpg",
     quantity: 1
   },
   {
     id: 2,
     name: "Cactus Family",
     price: "12€",
-    imageUrl: cactustwo,
+    imageUrl: "https://images2.imgbox.com/b4/f3/fXgm8qR7_o.jpg",
     quantity: 1
   },
   {
     id: 3,
     name: "Cute Cactus",
     price: "9€",
-    imageUrl: cactusthree,
+    imageUrl: "https://images2.imgbox.com/a8/8a/Doburfrm_o.jpg",
     quantity: 1
   },
   {
     id: 4,
     name: "Flowered",
     price: "17€",
-    imageUrl: cactusfour,
+    imageUrl: "https://images2.imgbox.com/75/7a/JHgR1ep3_o.jpg",
     quantity: 1
   },
   {
     id: 5,
     name: "Cactus Scrub",
     price: "10€",
-    imageUrl: cactusfive,
+    imageUrl: "https://images2.imgbox.com/ce/32/jlLqnfjq_o.jpg",
     quantity: 1
   },
   {
     id: 6,
     name: "Traditional",
     price: "11€",
-    imageUrl: cactussix,
+    imageUrl: "https://images2.imgbox.com/de/8c/PFe2b6Qv_o.jpg",
     quantity: 1
   },
   
